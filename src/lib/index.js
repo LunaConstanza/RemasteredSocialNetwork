9a168518cb8573001e3e81efc0dd8d48961f966a
LunaConstanza <lunagonb@gmail.com>
2023-08-21 10:45:44 -0400

    update cdn

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-app.js";
 import { firebaseConfig } from "./firebaseConfig.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,7 +13,7 @@ import {
     GoogleAuthProvider,
     signInWithPopup,
     signOut 
-} from "firebase/auth";
+} from "https://www.gstatic.com/firebasejs/10.2.0/firebase-auth.js";
 import { 
     getFirestore,
     addDoc,
@@ -28,8 +28,8 @@ import {
     updateDoc,
     arrayRemove,
     arrayUnion
-} from "firebase/firestore";
-import { getStorage, ref, uploadBytes, uploadBytesResumable } from "firebase/storage";
+} from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, uploadBytesResumable } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-storage.js";
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
