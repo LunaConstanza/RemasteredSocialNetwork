3dce0d25cfbf1318096e2e7adcc4a18d96aa9d7c
LunaConstanza <lunagonb@gmail.com>
2023-08-21 16:41:29 -0400

    deploy github pages

--- src/components/h&f.js
+++ src/components/h&f.js
@@ -1,9 +1,11 @@
+import img from "../img/logo.png"
+
 export const headerLogo = () => {
     const header = document.createElement("header");
     const logo = document.createElement("img");
     logo.classList.add("logoRS");
     logo.setAttribute("width", "200px");
-    logo.setAttribute("src", "../img/logo.png");
+    logo.setAttribute("src", img);
     logo.setAttribute("alt", "Logo red social");
   
     header.appendChild(logo);
