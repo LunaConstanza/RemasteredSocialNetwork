40139e21c1887a24759a88275f1ecef6f19138f5
LunaConstanza <lunagonb@gmail.com>
2023-08-21 12:44:05 -0400

    update build & preview

--- src/components/h&f.js
+++ src/components/h&f.js
@@ -1,9 +1,11 @@
+import imgLogo from "../img/logo.png"
+
 export const headerLogo = () => {
     const header = document.createElement("header");
     const logo = document.createElement("img");
     logo.classList.add("logoRS");
     logo.setAttribute("width", "200px");
-    logo.setAttribute("src", "./img/logo.png");
+    logo.setAttribute("src", imgLogo);
     logo.setAttribute("alt", "Lodo red social");
   
     header.appendChild(logo);
