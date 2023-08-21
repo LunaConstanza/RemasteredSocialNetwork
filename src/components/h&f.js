24373469d6a7658929abd5c7db3f1bcacc5b90d1
Luna González B <lunagonb@gmail.com>
2023-08-21 16:22:38 -0400

    update deploy

--- src/components/h&f.js
+++ src/components/h&f.js
@@ -1,11 +1,9 @@
-import img from "../img/logo.png"
-
 export const headerLogo = () => {
     const header = document.createElement("header");
     const logo = document.createElement("img");
     logo.classList.add("logoRS");
     logo.setAttribute("width", "200px");
-    logo.setAttribute("src", img);
+    logo.setAttribute("src", "../img/logo.png");
     logo.setAttribute("alt", "Logo red social");
   
     header.appendChild(logo);
